b381a4b42fb33a9425b9fd613f32d753066943fa
Baran Ozbek <ozbek.baran@gmail.com>
2025-01-03 14:47:06 +0300

    add js files to the root

---
+++ 01-gallery.js
@@ -0,0 +1,34 @@
+console.log('hello123');
+
+import './init.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { images } from '/goit-js-hw-09/assets/images.js';
+
+const galleryContainer = document.querySelector('.gallery');
+
+// Galeri öğelerini dinamik olarak oluşturma
+const galleryMarkup = images
+  .map(
+    ({ preview, original, description }) => `
+    <li class="gallery-item">
+      <a class="gallery-link" href="${original}">
+        <img
+          class="gallery-image"
+          src="${preview}"
+          alt="${description}"
+        />
+      </a>
+    </li>
+  `
+  )
+  .join('');
+
+// Galeri markup'u ekle
+galleryContainer.innerHTML = galleryMarkup;
+
+// SimpleLightbox başlatma
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
